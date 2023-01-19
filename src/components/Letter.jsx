ff97ea2a2616b745d56c0e7327c58734b759421b
Bhavesh S Singh <bhaveshsinghbiz@gmail.com>
2023-01-19 20:42:45 +0530

    style:layout done

---
+++ src/components/Letter.jsx
@@ -0,0 +1,13 @@
+const Letter = ({ letterPos, attemptVal }) => {
+  return (
+    <div
+      className="p-4 border border-gray-600 m-1 h-16 w-12
+      text-center text-xl font-bold uppercase
+ rounded-[4px]"
+    >
+      K
+    </div>
+  );
+};
+
+export default Letter;
---
+++ src/context/DarkModeContext.js
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+
+const first = useContext(second);
+
+createContext;
+
+// Dark Mode
